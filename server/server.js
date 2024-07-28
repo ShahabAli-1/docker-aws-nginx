22a57a6baef9c10f23da378bee333fe73b74bd6f
Shahab Ali <sali.bese20seecs@seecs.edu.pk>
2024-07-28 20:38:00 +0500

    frontend and mongo

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 const http = require("http");
 const SERVER_HOST = process.env.SERVER_HOST;
 const SERVER_PORT = process.env.SERVER_PORT;
-
+const MONGOURL = process.env.MONGOURL;
+const mongoose = require("mongoose");
 async function main() {
   try {
     // Create server and listen for requests
     const server = http.createServer();
+    await mongoose
+      .connect(MONGOURL)
+      .then(() => console.log("Connected to MongoDB."))
+      .catch((err) => console.log("Error connecting to DB."));
     server.on("request", async (req, res) => {
       res.end(JSON.stringify({ SERVER_HOST, SERVER_PORT }));
     });
